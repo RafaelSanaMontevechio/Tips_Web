f09d27e6518d11f956c7e6545ddcbab20a61c4e6
rafaelsanamontevechio <rafael_sana@hotmail.com>
2018-03-11 11:15:11 -0300

    simple search component

--- Tips/src/app/tips/pages/pages.module.ts
+++ Tips/src/app/tips/pages/pages.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SimpleSearchComponent } from './simple-search/simple-search.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ SimpleSearchComponent]
 })
 export class PagesModule { }
