3835ad6872f140e9c9247541ad5bdb6a797edfa4
RafaelSanaMontevechio <rafael_sana@hotmail.com>
2018-03-16 14:17:33 -0300

    criado rota para o componente de resultado das buscas

--- Tips/src/app/tips/search/search.routing.module.ts
+++ Tips/src/app/tips/search/search.routing.module.ts
@@ -3,10 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { SimpleSearchComponent } from './simple-search/simple-search.component';
 import { CompleteSearchComponent } from './complete-search/complete-search.component';
+import { ResultComponent } from './result-search/result.component';
 
 const SEARCH_ROUTES: Routes = [
     { path: '', component: SimpleSearchComponent },
-    { path: './complete-Search', component: CompleteSearchComponent }
+    { path: 'complete-Search', component: CompleteSearchComponent },
+    { path: 'result-search', component: ResultComponent },
+    { path: 'result-search', redirectTo: '/result-search', pathMatch: 'full' }
 ];
 
 @NgModule({
